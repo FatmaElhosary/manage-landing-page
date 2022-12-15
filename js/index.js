6ac222e097ef1a9f77e60321935e4ce6734fc1cc
MohamedAshraf2 <mohamashraf144@gmail.com>
2022-12-15 12:35:20 +0200

    updating dark mode style and  creating the footer

--- js/index.js
+++ js/index.js
@@ -10,3 +10,47 @@ $(".change").on("click", function () {
     }
 });
 ////////////////////Dark Mode Code////////////////////////////////
+
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('dark');
+})
+
+
+////////////////////Email validation//////////////////////////////////////////////////
+var valid = document.getElementById('valid')
+var invalid = document.getElementById('invalid')
+
+function ValidateEmail(input) {
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  
+    if (input.value.match(validRegex)) {
+  
+    //   alert("Valid email address!");
+  
+    //   document.form1.text1.focus();
+
+   valid.style.display = 'block'
+   invalid.style.display = 'none'
+    
+      return true;
+  
+    } else {
+  
+    //   alert("Invalid email address!");
+  
+    //   document.form1.text1.focus();
+    valid.style.display = 'none'
+    invalid.style.display = 'block'
+      return false;
+  
+    }
+  
+  }
+
+ var val = document.getElementById('update');
+
+
+  document.getElementById('submit').addEventListener('click' ,()=>{ValidateEmail(val)} )
