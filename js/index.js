9b4977075dfd2d7fe65ad0cf767d9b92c4741f23
FatmaElhosary <engfatmafathy@gmail.com>
2022-12-17 20:25:38 +0200

    made carousel in large screens

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const menu=document.querySelector('.menu');
+ const menu=document.querySelector('.menu');
 document.querySelectorAll('.menu--wrapper').forEach(carousel=>{
 const items=carousel.querySelectorAll('.menu--item');
 const buttonsHtml=Array.from(items,()=>{
@@ -27,4 +27,48 @@ button.addEventListener('click',()=>{
 //select the first item in page load
 items[0].classList.add('item-selected');
 buttons[0].classList.add("carousel-button-selected");
+}); 
+////////////////////////////////////////////////////////////////////
+let sliderContainer = document.querySelector('.menu');
+let innerSlider = document.querySelector('.menu--wrapper');
+
+let pressed = false;
+let startX;
+let x;
+
+const checkBoundary = () => {
+    let outer = sliderContainer.getBoundingClientRect();
+    let inner = innerSlider.getBoundingClientRect();
+
+    if (parseInt(innerSlider.style.left) > 0) {
+        innerSlider.style.left = "0px";
+    }
+   else if (inner.right < outer.right) {
+        innerSlider.style.left = `-${inner.width - outer.width}px`;
+    }
+    
+};
+sliderContainer.addEventListener("mousedown", (e) => {
+    pressed = true;
+    startX = e.offsetX - innerSlider.offsetLeft;
+    sliderContainer.style.cursor = "grabbing";
+});
+
+sliderContainer.addEventListener("mouseenter", () => {
+    sliderContainer.style.cursor = "grab";
+});
+
+sliderContainer.addEventListener("mouseup", () => {
+    sliderContainer.style.cursor = "grab";
+    pressed = false;
+});
+
+sliderContainer.addEventListener("mousemove", (e) => {
+    if (!pressed) return;
+    e.preventDefault();
+
+    x = e.offsetX;
+
+    innerSlider.style.left = `${x - startX}px`;
+    checkBoundary();
 });
